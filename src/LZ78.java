a7f79e792648e953a9b680ed60d8046bd705f486
Youssef Ahmed <youssef9300@hotmail.com>
2021-01-10 05:03:48 +0200

    Refractoring

--- src/LZ78.java
+++ src/LZ78.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class LZ78{
-    public String ReadFromFile (String Path) throws IOException {
+public class LZ78 {
+    public String ReadFromFile(String Path) throws IOException {
         String content = null;
         File file = new File(Path);
         FileReader reader = null;
@@ -26,11 +26,12 @@ public class LZ78{
         }
         return content;
     }
-    public void WriteToFile(String content,String name){
+
+    public void WriteToFile(String content, String name) {
         BufferedWriter writer = null;
-        String fileName = name+".txt" ;
+        String fileName = name + ".txt";
         File logFile = new File(fileName);
-        try{
+        try {
             writer = new BufferedWriter(new FileWriter(logFile));
             writer.write(content);
         } catch (Exception e) {
@@ -38,125 +39,116 @@ public class LZ78{
         } finally {
             try {
                 writer.close();
-            } catch (Exception e) {}
+            } catch (Exception e) {
+            }
 
         }
     }
-    public int Exist(String chars, Vector<String> d){
+
+    public int Exist(String chars, Vector<String> d) {
         int len = d.size();
-        for (int i = 0; i < len ; i++)
-        {
+        for (int i = 0; i < len; i++) {
             if (chars.equals(d.get(i)))
-                return i+1;
+                return i + 1;
         }
         return -1;
     }
-    public void Compress(String path , String name) {
+
+    public void Compress(String path, String name) {
         String text = new String();
-        String compressed = new String ("") ;
-        Vector<String> d = new Vector<String>() ;
+        String compressed = new String("");
+        Vector<String> d = new Vector<String>();
 
         try {
             text = ReadFromFile(path);
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
 
         int len = text.length();
-        for (int i = 0; i<len; i++)
-        {
-            String curr =new String("");
-            curr += (text.toCharArray()[i]) ;
-            int ptr = 0 ;
+        for (int i = 0; i < len; i++) {
+            String curr = new String("");
+            curr += (text.toCharArray()[i]);
+            int ptr = 0;
             String next = "";
-            while (Exist(curr, d)!=-1)
-            {
+            while (Exist(curr, d) != -1) {
                 i++;
-                ptr = Exist(curr , d) ;
-                if (i==len)
-                    break ;
+                ptr = Exist(curr, d);
+                if (i == len)
+                    break;
                 curr += text.toCharArray()[i];
             }
-            if(Exist(curr,d)==-1)
-                ptr=ptr;
+            if (Exist(curr, d) == -1)
+                ptr = ptr;
             else
                 i--;
 
             d.addElement(curr);
 
 
-            if (i==len)
-                next += "NULL" ;
-            else
-            {
+            if (i == len)
+                next += "NULL";
+            else {
                 next += text.toCharArray()[i];
             }
 
-            String tag = new String("<" + ptr +","+ next + ">");
-            compressed+=tag;
+            String tag = new String("<" + ptr + "," + next + ">");
+            compressed += tag;
         }
 
-        WriteToFile(compressed , name);
+        WriteToFile(compressed, name);
 
     }
-    public void Decompress(String path , String name)
-    {
+
+    public void Decompress(String path, String name) {
         String text = new String();
-        String decompressed = new String ("") ;
-        Vector<String> d = new Vector<String>() ;
+        String decompressed = new String("");
+        Vector<String> d = new Vector<String>();
 
         try {
             text = ReadFromFile(path);
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
 
         int len = text.length();
 
-        for (int i=0 ; i<len ; i++)
-        {
-            int ptr = 0 , length = 0 ;
-            String next = "" ;
-            String temp ="";
-            i++ ;
-            while (text.toCharArray()[i]!=',')
-            {
+        for (int i = 0; i < len; i++) {
+            int ptr = 0, length = 0;
+            String next = "";
+            String temp = "";
+            i++;
+            while (text.toCharArray()[i] != ',') {
                 temp += text.toCharArray()[i];
-                i++ ;
+                i++;
             }
-            i++ ;
+            i++;
             ptr = Integer.parseInt(temp);
-            if (i==len-5)
-            {
-                for (int k=i ; k<i+4 ; k++)
-                {
-                    next +=text.toCharArray()[k];
+            if (i == len - 5) {
+                for (int k = i; k < i + 4; k++) {
+                    next += text.toCharArray()[k];
                 }
-                i=len-1 ;
-            }
-            else
-            {
+                i = len - 1;
+            } else {
                 next += text.toCharArray()[i];
-                i++ ;
+                i++;
             }
 
-            if (ptr == 0)
-            {
-                decompressed += next ;
+            if (ptr == 0) {
+                decompressed += next;
                 d.add(next);
-            }
-            else
-            {
+            } else {
                 if (!next.equals("NULL")) {
 
                     temp = d.elementAt(ptr - 1);
                     temp += next;
                     decompressed += temp;
                     d.add(temp);
-                }
-                else{
-                    decompressed+=d.elementAt(ptr-1);
+                } else {
+                    decompressed += d.elementAt(ptr - 1);
                 }
 
             }
         }
-        WriteToFile(decompressed , name);
+        WriteToFile(decompressed, name);
 
     }
 
--- src/main.java
+++ src/main.java
@@ -1,7 +1,8 @@
 public class main {
-    public static void main(String args[]){
-        LZ78 lz78=new LZ78();
-        lz78.Compress("C:\\Users\\RCSC\\Documents\\GitHub\\LZ78\\test.txt", "compressed");
-        lz78.Decompress("C:\\Users\\RCSC\\Documents\\GitHub\\LZ78\\compressed.txt", "decompressed");
+    public static void main(String args[]) {
+        LZ78 lz78 = new LZ78();
+        //Take Care of the Path of the project
+        lz78.Compress("D:\\Java Repos\\LZ78\\test.txt", "compressed");
+        lz78.Decompress("D:\\Java Repos\\LZ78\\compressed.txt", "decompressed");
     }
 }
